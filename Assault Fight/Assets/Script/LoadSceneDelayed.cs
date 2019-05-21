9d01593228ed8b4352e1c06a3069622122984012
maciejg <maciej.gora01@gmail.com>
2019-05-22 01:08:35 +0200

    Undestroyable through scenes music player

---
+++ Assault Fight/Assets/Script/LoadSceneDelayed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneDelayed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start invoke method");
+        Invoke("ShowLevel1", 5);
+    }
+    private void ShowLevel1() {
+        //Debug.Log("It's time to show the next level");
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assault Fight/Assets/Script/UndestroyableMusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UndestroyableMusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake() {
+        GameObject[] musicGameObjects = GameObject.FindGameObjectsWithTag("MainMusicPlayer");
+        for (int i=0; i<musicGameObjects.Length; i++) {
+            if (i >= 1) {
+                Destroy(musicGameObjects[i]);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
